ad39baa4a8199f1ce9d43d33c6bf05cf46b47bb7
GitHub Action <noreply@github.com>
2021-07-23 10:53:21 +0000

    Update sub-web from Soroke/sub-web

---
+++ sub-web/precache-manifest.a5b1ddd59fde86693949aa03aaaa4b06.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72cd7d7b6923016ad755aed91ed7ed31",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "13fed48a0d569bf66a38",
+    "url": "/css/app.6a5dab7f.css"
+  },
+  {
+    "revision": "8767e6a109ad7b9fbebb",
+    "url": "/css/chunk-vendors.1019b600.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "4961ada2ad4bcb81343098c54423a408",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13fed48a0d569bf66a38",
+    "url": "/js/app.c95c886d.js"
+  },
+  {
+    "revision": "67a50ea6ca0715ac9f10",
+    "url": "/js/chunk-258412ab.ed0ef504.js"
+  },
+  {
+    "revision": "8767e6a109ad7b9fbebb",
+    "url": "/js/chunk-vendors.147e3762.js"
+  },
+  {
+    "revision": "417c67dfde0c71bc33e953d4e7d635f8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
